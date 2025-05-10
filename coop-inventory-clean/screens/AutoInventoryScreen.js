dd0a346e8a63bd053b51b4b415ecb49c919e11c6
VBN3009 <144623629+VBN3009@users.noreply.github.com>
2025-05-09 19:05:45 -0500

    Working auto inventory

--- coop-inventory-clean/screens/AutoInventoryScreen.js
+++ coop-inventory-clean/screens/AutoInventoryScreen.js
@@ -1,7 +1,21 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
 export default function AutoInventoryScreen() {
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Auto Inventory (Color Scan) Screen</Text>
+      <Text style={styles.text}>Auto Inventory Screen</Text>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  text: {
+    fontSize: 20,
+  },
+});
